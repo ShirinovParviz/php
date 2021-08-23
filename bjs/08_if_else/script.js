1a6732ba2a3740ba7cb3a2de317885e37a3e8bf6
ShirinovParviz <84585794+ShirinovParviz@users.noreply.github.com>
2021-08-23 17:37:26 +0300

    master

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -29,6 +29,7 @@ document.getElementById('btnOver').addEventListener('click', function () {
 
             answerField.innerText = answerPhrase;
             gameRun = false;
+
         } else {
             minValue = answerNumber + 1;
             answerNumber = Math.floor((minValue + maxValue) / 2);
@@ -40,7 +41,6 @@ document.getElementById('btnOver').addEventListener('click', function () {
 });
 
 
-
 document.getElementById('btnLess').addEventListener('click', function () {
     if (gameRun) {
         if (minValue === maxValue) {
@@ -48,20 +48,23 @@ document.getElementById('btnLess').addEventListener('click', function () {
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
-
             answerField.innerText = answerPhrase;
             gameRun = false;
+
         } else {
-            minValue = answerNumber + 1;
-            answerNumber = Math.floor((maxValue - minValue) / 2);
+            maxValue = answerNumber - 1;
+            answerNumber = Math.floor((maxValue + minValue - Math.round(Math.random())) / 2 + 1);
             orderNumber++;
-            
+
             orderNumberField.innerText = orderNumber;
             answerField.innerText = `Вы загадали число ${answerNumber}?`;
         }
+
+
+
+
     }
 });
-
 
 
 document.getElementById('btnEqual').addEventListener('click', function () {
